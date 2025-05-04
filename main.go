b0c39400ac110c12b56566270523a05055d9b3de
MinhAnh553 <huynhminhanh553@gmail.com>
2025-05-04 07:32:49 +0700

    fix lỗi Quic (cơ bản)

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+
+	"github.com/qtai2901/new_xrayr/cmd"
+)
+
+func main() {
+	if err := cmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
